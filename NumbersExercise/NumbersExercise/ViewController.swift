d4ff0c8764252d44612f7770410614e01f6811a5
BarkinSungu <barkinsungu@gmail.com>
2023-07-20 17:17:38 +0300

    Digit to text switch added

--- NumbersExercise/NumbersExercise/ViewController.swift
+++ NumbersExercise/NumbersExercise/ViewController.swift
@@ -14,10 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var acceptButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
     
-    let numbersByDigit = ["1","2","3"]
-    let numbersInWriting = ["one", "two", "three"]
+    let numbersByDigit = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+        "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+        "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+        "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+        "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+        "61", "62", "63", "64", "65", "66", "67", "68", "69", "70",
+        "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
+        "81", "82", "83", "84", "85", "86", "87", "88", "89", "90",
+        "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"]
+    let numbersInWriting = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+        "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine", "thirty",
+        "thirty one", "thirty two", "thirty three", "thirty four", "thirty five", "thirty six", "thirty seven", "thirty eight", "thirty nine", "forty",
+        "forty one", "forty two", "forty three", "forty four", "forty five", "forty six", "forty seven", "forty eight", "forty nine", "fifty",
+        "fifty one", "fifty two", "fifty three", "fifty four", "fifty five", "fifty six", "fifty seven", "fifty eight", "fifty nine", "sixty",
+        "sixty one", "sixty two", "sixty three", "sixty four", "sixty five", "sixty six", "sixty seven", "sixty eight", "sixty nine", "seventy",
+        "seventy one", "seventy two", "seventy three", "seventy four", "seventy five", "seventy six", "seventy seven", "seventy eight", "seventy nine", "eighty",
+        "eighty one", "eighty two", "eighty three", "eighty four", "eighty five", "eighty six", "eighty seven", "eighty eight", "eighty nine", "ninety",
+        "ninety one", "ninety two", "ninety three", "ninety four", "ninety five", "ninety six", "ninety seven", "ninety eight", "ninety nine", "one hundred"]
     var correctAnsver = ""
     var score = 0
+    var switchNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +47,8 @@ class ViewController: UIViewController {
     }
     
     func askQuestion(){
-        var switchNumber = Int.random(in: 1..<3)
-        var questionNumber = Int.random(in: 0..<3)
+    //    var switchNumber = Int.random(in: 0..<3)
+        var questionNumber = Int.random(in: 0..<100)
         var questionArray = [String]()
         var answerArray = [String]()
         
@@ -53,6 +74,17 @@ class ViewController: UIViewController {
             trueAnswer()
         }else{
             wrongAnswer()
+        }
+    }
+    
+    @IBAction func DigitToTextSwitch(_ sender: UISwitch) {
+        score = score - 1 //askQuestion score u artırır soru çözülmeden artmasını engelemek için
+        if sender.isOn{
+            switchNumber = 0
+            askQuestion()
+        }else{
+            switchNumber = 1
+            askQuestion()
         }
     }
     
